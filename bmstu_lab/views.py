028539eb05696a2d6a74c8420f51c82533758f38
lovesosa420 <trappapa420@mail.ru>
2022-10-10 12:46:02 +0300

    lab1

--- bmstu_lab/views.py
+++ bmstu_lab/views.py
@@ -5,18 +5,19 @@ def welcome(request):
     return render(request, 'welcome.html')
 
 
-def GetOrders(request):
-    return render(request, 'sneakers.html', {'data' : {
+def Sneakers(request):
+    return render(request, 'sneakers.html', {'data': {
         'sneakers': [
-            {'title': 'Dark Iris', 'id': 'DarkIris', 'release': '10.09.2019'},
-            {'title': 'Katrina', 'id': 'Katrina', 'release': '10.09.2018'},
-            {'title': 'Racer Blue', 'id': 'RacerBlue', 'release': '10.09.2017'},
+            {'title': 'Dark Iris', 'id': 'DarkIris', 'photo': 'DarkIris.jpg'},
+            {'title': 'Katrina', 'id': 'Katrina', 'photo': 'Katrina.jpg'},
+            {'title': 'Racer Blue', 'id': 'RacerBlue', 'photo': 'RacerBlue.jpg'},
         ]
     }})
 
 
-def GetOrder(request, id, release):
+def Model(request, id, photo):
+    photo = 'images/' + photo
     return render(request, 'unit.html', {'data' : {
         'id': id,
-        'release': release,
+        'photo': photo
     }})
--- Лаба1/urls.py
+++ Лаба1/urls.py
@@ -5,6 +5,6 @@ from bmstu_lab import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.welcome),
-    path('sneakers/', views.GetOrders, name='catalog_url'),
-    path('sneakers/<id>/<release>',  views.GetOrder, name='sneaker_url'),
+    path('sneakers/', views.Sneakers, name='catalog_url'),
+    path('sneakers/<id>/<photo>',  views.Model,  name='sneaker_url'),
 ]
